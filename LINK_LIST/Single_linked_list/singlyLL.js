f9eff9054e65c5143195e4da6b108e0bc1b3b457
Hemang Patel <hemang9705@gmail.com>
2023-12-21 21:41:57 +0530

    singly linked list

--- LINK_LIST/Single_linked_list/singlyLL.js
+++ LINK_LIST/Single_linked_list/singlyLL.js
@@ -24,6 +24,9 @@ class LinkedList {
         node.link = ptr.link;
         ptr.link = node;
         arr.push(val);
+        arr.sort(function (a, b) {
+          return a - b;
+        });
         console.log(arr);
         return;
       }
@@ -31,10 +34,13 @@ class LinkedList {
     }
     ptr.link = node;
     arr.push(val);
-    console.log(arr);
+    arr.sort(function (a, b) {
+      return a - b;
+    });
   }
 
   delete(val) {
+    val = Number.parseInt(val);
     if (this.head === null) {
       alert("can't delete from empty linkedlist");
       return;
@@ -44,7 +50,7 @@ class LinkedList {
       pred = this.head;
 
     while (ptr.link !== null) {
-      if (ptr.data == val) {
+      if (ptr.data === val) {
         pred.link = ptr.link;
         arr.splice(arr.indexOf(val), 1);
         console.log(arr);
@@ -53,56 +59,122 @@ class LinkedList {
       pred = ptr;
       ptr = ptr.link;
     }
-    alert("Node not found");
+
+    if (ptr.data === val) {
+      pred.link = ptr.link;
+      arr.pop();
+      console.log(arr);
+      return;
+    }
+    alert("Node not found");
   }
 
-  search(n) {
-      let ptr = this.head;
+  async search(n) {
+    let flag = false;
+    let item = 0;
+    let oddrow = true;
+    let item_num = 0;
+    let first = true;
+    let new_row = false;
 
-      while (ptr !== null) {
-          if (ptr.data == n) {
-            alert("found in list");
-            break;
-          }
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === Number.parseInt(n)) {
+        await setColor(item, oddrow,"lightgreen");
+        alert("found in list");
+        flag = true;
+        displayNodes();
+        break;
       }
-      alert("Node not found in list!");
-  }
-
-  printLL() {
-    let ptr = this.head,
-      str = "";
-
-    while (ptr !== null) {
-      str += ptr.data + " ";
-      ptr = ptr.link;
+      await setColor(item, oddrow);
+      item++;
+      item_num++;
+      if (item_num == 1) {
+        first = false;
+        new_row = false;
+      }
+      if (item_num == 8) {
+        oddrow = false;
+      } else if (item_num == 16) {
+        item_num = 0;
+        new_row = true;
+        oddrow = true;
+      }
+      //compare the data to find the node
     }
 
-    console.log(str);
+    if (flag == false) {
+      alert("Node not found in list!");
+    }
+
+    async function setColor(item, oddrow,last_color) {
+      if (oddrow == true) {
+        if (item != 0 && new_row == true) {
+          let oldchild = document.getElementById(`item${item - 1}`).children;
+          oldchild[1].style.backgroundColor = "aqua";
+          oldchild[0].childNodes[0].style.backgroundColor = "aqua";
+        } else if (item != 0 && first == false) {
+          let oldchild = document.getElementById(`item${item - 1}`).children;
+          oldchild[0].style.backgroundColor = "aqua";
+          oldchild[1].childNodes[0].style.backgroundColor = "aqua";
+        }
+        let newchild = document.getElementById(`item${item}`).children;
+        newchild[0].style.backgroundColor = last_color??"red";
+        newchild[1].childNodes[0].style.backgroundColor = last_color??"red";
+        await sleep(700);
+        if (last_color != null) {
+          await sleep(1100);
+        }
+      } else {
+        if (item % 8 == 0) {
+          let oldchild = document.getElementById(`item${item - 1}`).children;
+          oldchild[0].style.backgroundColor = "aqua";
+          oldchild[1].childNodes[0].style.backgroundColor = "aqua";
+        } else {
+          let oldchild = document.getElementById(`item${item - 1}`).children;
+          oldchild[1].style.backgroundColor = "aqua";
+          oldchild[0].childNodes[0].style.backgroundColor = "aqua";
+        }
+        let newchild = document.getElementById(`item${item}`).children;
+        newchild[1].style.backgroundColor = last_color ?? "red";
+        newchild[0].childNodes[0].style.backgroundColor = last_color ?? "red";
+        await sleep(700);
+        if(last_color!=null)
+        {
+          await sleep(1000);
+        }
+      }
+    }
+    function sleep(ms) {
+      return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+      });
+    }
+    let oldchild = document.getElementById(`item${item - 1}`).children;
+    oldchild[0].style.backgroundColor = "aqua";
+    oldchild[1].childNodes[0].style.backgroundColor = "aqua";
   }
 }
 sl = new LinkedList();
 //clicking event
 document.getElementById("btnSearch").addEventListener("click", () => {
   let val = document.getElementById("searchInput").value;
-  sl.search(val);
-  val="";
+  sl.search(Number.parseInt(val));
+  val = "";
 });
 document.getElementById("btnInsert").addEventListener("click", () => {
   let val = document.getElementById("insertInput");
-  if (val != "" && val != NaN) {
-    sl.insert(val.value);
+  if (val.value != "" && val.value != NaN) {
+    sl.insert(Number.parseInt(val.value));
   }
-  val.value="";
+  val.value = "";
   displayNodes();
 });
 document.getElementById("btnDelete").addEventListener("click", () => {
   let val = document.getElementById("deleteInput");
-  sl.delete(val.value);
+  sl.delete(Number.parseInt(val.value));
   displayNodes();
-  val.value="";
+  val.value = "";
 });
-
-
 
 function displayNodes() {
   let parent = document.getElementById("linked-list-container");
@@ -118,10 +190,10 @@ function displayNodes() {
     if (a == arr.length - 1) {
       if (row_num % 2 == 1) {
         let old_row = document.getElementById(`row${row_num}`);
-        old_row.innerHTML += `<div class="item"><div class="value">${arr[a]}</div><div class="null"><div></div></div><div>`;
+        old_row.innerHTML += `<div class="item" id="item${a}"><div class="value">${arr[a]}</div><div class="null"><div></div></div><div>`;
       } else {
         let old_row = document.getElementById(`row${row_num}`);
-        old_row.innerHTML += `<div class="item"><div class="null"><div></div></div><div class="value">${arr[a]}</div></div>`;
+        old_row.innerHTML += `<div class="item" id="item${a}"><div class="null"><div></div></div><div class="value">${arr[a]}</div></div>`;
       }
     } else if (listCount > 0 && listCount <= 7) {
       let old_row = document.getElementById(`row${row_num}`);
