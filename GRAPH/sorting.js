fe24e28d2a4583bfab24af05c3b337fb1af75894
Hemang Patel <hemang9705@gmail.com>
2024-01-16 22:54:54 +0530

    meta keywords added to all files

--- GRAPH/sorting.js
+++ GRAPH/sorting.js
@@ -1,7 +1,8 @@
-document.getElementById('sort').addEventListener('click',()=>
+let arr=[];
+document.getElementById('sort').addEventListener('click',async ()=>
 {
 	let value=document.getElementById('input').value;
-	let arr=value.split(',');
+	arr=value.split(',');
 	for(let a=0;a<arr.length;a++)
 	{
 		arr[a]=Number.parseFloat(arr[a]);
@@ -9,15 +10,17 @@ document.getElementById('sort').addEventListener('click',()=>
 	let min=Math.min(...arr);
 	console.log(arr);
 	//making the division for sort
+	document.getElementById('items').innerHTML="";
 	for(let a=0;a<arr.length;a++)
 	{
 		let parent=document.getElementById('items');
 		let child=document.createElement('div');
 		child.setAttribute('class','item');
+		child.setAttribute('id',`item_${arr[a]}`);
 		child.innerHTML=arr[a];
-		parent.append(child);
+		await parent.append(child);
 	}
-	bubbleSort(arr,min);
+	await bubbleSort(arr,min);
 });
 
 function bubbleSort(arr)
@@ -31,8 +34,41 @@ function bubbleSort(arr)
 				let tmp=arr[a];
 				arr[a]=arr[b];
 				arr[b]=tmp;
+				swapping(arr[a],arr[b]);
 			}
 		}
 	}
 	console.log(arr);
+}
+async function swapping(first,second)
+{
+	let starting=arr.indexOf(first);
+	let ending=arr.indexOf(second);
+	console.log(starting,ending);
+
+	let toppx=0;
+	while(true)
+	{
+		document.getElementById(`item_${first}`).style.top=toppx;
+		toppx++;
+		await sleep(20);
+		if(toppx==100)
+		{
+			break;
+		}
+	}
+}
+function sleep(ms)
+{
+	return new Promise((resolve,reject)=>
+	{
+		try{
+			setTimeout(resolve,ms);
+		}
+		catch(arr)
+		{
+			console.log(reject,arr);
+			alert("Sorry, we've got error while aniamtion.");
+		}
+	})
 }
